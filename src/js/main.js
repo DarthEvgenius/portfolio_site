32a04baa026725c3908bc0bf74404baaf724de2b
Eugene Zinin <e.zinin@joki-joya.ru>
2025-07-17 15:40:41 +0300

    style fixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,25 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+import html2pdf from 'html2pdf.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('download-pdf');
+
+  button.addEventListener('click', () => {
+    const element = document.body; // нужный контейнер
+
+    // Настройки pdf
+    const opt = {
+      margin: 0.5,
+      filename: 'page.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+  });
+});
+
